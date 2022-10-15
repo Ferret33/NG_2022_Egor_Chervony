ac7ad4ad3ac68e1df88760b8d6016e8a65583e11
Egor Chervony <chervony.egor@gmail.com>
2022-10-15 22:29:07 +0300

    First try to push

--- Lesson1/Lesson1.py
+++ Lesson1/Lesson1.py
@@ -2,5 +2,4 @@ exampleNumber = input("Enter variable: ")
 #print -- show smth on screan
 #type -- return type of variable
 #int -- dynamic cast
-# comment
 print(type(int(exampleNumber)))
--- Lesson1/lesson2.py
+++ Lesson1/lesson2.py
@@ -2,5 +2,5 @@ firstNumberForSum = int(input("Enter first number: "))
 secondNumberFoeSum = int(input("Enter second number: "))
 print("==============================================")
 print("Sum " + str(firstNumberForSum + secondNumberFoeSum))
-#coment
+
 

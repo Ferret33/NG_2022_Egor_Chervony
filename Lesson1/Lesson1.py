17bd8ba08caf6a4983f05418df7cb022aaff05d0
Yehor Chervony <chervony.egor@gmail.com>
2022-10-18 15:54:52 +0300

    .

--- Lesson1/Lesson1.py
+++
@@ -1,6 +0,0 @@
-exampleNumber = input("Enter variable: ")
-#print -- show smth on screan
-#type -- return type of variable
-#int -- dynamic cast
-# coment
-print(type(int(exampleNumber)))
--- Lesson1/lesson2.py
+++
@@ -1,6 +0,0 @@
-firstNumberForSum = int(input("Enter first number: "))
-secondNumberFoeSum = int(input("Enter second number: "))
-print("==============================================")
-print("Sum " + str(firstNumberForSum + secondNumberFoeSum))
-
-

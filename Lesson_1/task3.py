b7881beb6faab1d9be6d70378f260bc0ffdf939c
Yehor Chervony <chervony.egor@gmail.com>
2022-10-19 17:43:06 +0300

    Lection 1 - Task 3

--- Lesson_1/task3.py
+++ Lesson_1/task3.py
@@ -1,4 +1,6 @@
-import time
-
-introducedSeconds = int(input("Enter the number of seconds: "))
-print(time.strftime("%d, %H:%M:%S", time.gmtime(0+introducedSeconds)))
+entersec = int(input("Enter the number of seconds: "))
+seconds = entersec%60
+minutes = (entersec//60)%60
+hours = ((entersec//60)//60)%24
+days = ((entersec//60)//60)//24
+print(str(days)+":"+str(hours)+":"+str(minutes)+":"+str(seconds))

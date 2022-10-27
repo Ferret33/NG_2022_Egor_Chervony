6ee38c2a0635b53a98d9e0effed0654daf505607
Yehor Chervony <chervony.egor@gmail.com>
2022-10-27 15:26:52 +0300

    Lection 2 - Task 3 Reverse hill of numbers

--- Lesson_2/task3.py
+++ Lesson_2/task3.py
@@ -2,12 +2,13 @@ numb= int(input("Enter your number: "))
 numberlist=[]
 i=0
 while i<numb:
-    numberlist.insert(i, i+1)
+    numberlist.insert(i, str(i+1))
     i+=1
 numberlist.reverse()  
 k=0
-print(numberlist)
+
 while k<numb:
+    print(" ".join(numberlist))
     numberlist.remove(numberlist[0])
     k+=1
     

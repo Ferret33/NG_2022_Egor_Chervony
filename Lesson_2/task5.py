a84bf7ec2a02da78029933fff9f1474181a665c4
Yehor Chervony <chervony.egor@gmail.com>
2022-10-27 16:53:29 +0300

    Lection 2 - Task 5 List Max Min, Sum without max, min

---
+++ Lesson_2/task5.py
@@ -0,0 +1,7 @@
+enterednumber= input("Enter list: ")
+splitednumber= enterednumber.split(", ")
+splitednumber = list(map(int, splitednumber))
+splitednumber.sort()
+print("Min value: "+ str(splitednumber[0]))
+print("Max value: "+ str(splitednumber[-1]))
+print("Sum list without Max,Min: "+ str(sum(splitednumber)-splitednumber[0]-splitednumber[-1]))

7b12ae16f2a842058e365544264643ce1ae46c46
Yehor Chervony <chervony.egor@gmail.com>
2022-11-04 15:25:27 +0200

    Lection 2 - Task 3 Reverse hill of numbers | Code fixed

--- Lesson_2/task3.py
+++ Lesson_2/task3.py
@@ -1,14 +1,10 @@
 enternumber= int(input("Enter your number: "))
-numberlist=[]
-iteration=0
-while iteration<enternumber:
-    numberlist.insert(iteration, str(iteration+1))
-    iteration+=1
-numberlist.reverse()  
 iteration=0
 
-while iteration<enternumber:
-    print(" ".join(numberlist))
-    numberlist.remove(numberlist[0])
-    iteration+=1
-    
+while iteration!=enternumber :
+    while iteration<enternumber:
+        print(enternumber-iteration,end=" ")
+        iteration+=1
+    print(end="\n")
+    iteration=0
+    enternumber-=1

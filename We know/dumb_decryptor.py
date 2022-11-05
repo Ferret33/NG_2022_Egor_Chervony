5da434abc82e73e1b099216f09bc77c5d86f1ada
Yehor Chervony <chervony.egor@gmail.com>
2022-11-05 13:46:32 +0200

    Quis custodiet ipsos cust?

--- We know/dumb_decryptor.py
+++ We know/dumb_decryptor.py
@@ -1,5 +1,63 @@
 enterline= input("Enter line to encrypt: ")
-translaterline=str.maketrans('abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-                             'nopqrstuvwxyzabcdefghijklm NOPQRSTUVWXYZABCDEFGHIJKLM')
-print("=========================================================")
-print (enterline.translate(translaterline))
+translate={
+        "a":"n",
+        "b":"o",
+        "c":"p",
+        "d":"q",
+        "e":"r",
+        "f":"s",
+        "g":"t",
+        "h":"u",
+        "i":"v",
+        "j":"w",
+        "k":"x",
+        "l":"y",
+        "m":"z",
+        "n":"a",
+        "o":"b",
+        "p":"c",
+        "q":"d",
+        "r":"e",
+        "s":"f",
+        "t":"g",
+        "u":"h",
+        "v":"i",
+        "w":"j",
+        "x":"k",
+        "y":"l",
+        "z":"m",
+        "A":"N",
+        "B":"O",
+        "C":"P",
+        "D":"Q",
+        "E":"R",
+        "F":"S",
+        "G":"T",
+        "H":"U",
+        "I":"V",
+        "J":"W",
+        "K":"X",
+        "L":"Y",
+        "M":"Z",
+        "N":"A",
+        "O":"B",
+        "P":"C",
+        "Q":"D",
+        "R":"E",
+        "S":"F",
+        "T":"G",
+        "U":"H",
+        "V":"I",
+        "W":"J",
+        "X":"K",
+        "Y":"L",
+        "Z":"M"
+}
+treanstaedline=""
+for element in enterline:
+    if element in translate.keys():
+        treanstaedline += (translate[element])
+    else:
+        treanstaedline += element
+print("===================================================")
+print(treanstaedline)

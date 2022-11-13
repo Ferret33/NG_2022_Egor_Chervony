f84279af810eff84f62ec98275387b271160ca22
Yehor Chervony <chervony.egor@gmail.com>
2022-11-13 19:26:34 +0200

    Quis custodiet ipsos cust?

--- We know/dumb_decryptor.py
+++ We know/dumb_decryptor.py
@@ -1,9 +1,10 @@
 enterline= input("Enter line to encrypt: ").lower()
 translate="abcdefghijklmnopqrstuvwxyz"
+decryptkey = int(input("Enter decrypt key (ROT13 - 13): "))
 treanstaedline=""
 for element in enterline:
     if element in translate:
-        treanstaedline += (translate[translate.index(element)-13])
+        treanstaedline += (translate[translate.index(element)-decryptkey])
     else:
         treanstaedline += element
 print("===================================================")

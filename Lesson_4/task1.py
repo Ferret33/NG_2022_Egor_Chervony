97ddb341c8413c81398f99a0ea15ba0d5e67c9c8
Yehor Chervony <chervony.egor@gmail.com>
2022-11-14 15:05:46 +0200

    Lection 4 - Task 1 Computer info to txt | Code Fixed options added

--- Lesson_4/task1.py
+++ Lesson_4/task1.py
@@ -3,70 +3,72 @@ import platform
 import psutil
 import inquirer
 import cpuinfo
-from inquirer.themes import Default
+import socket
 
 console=Console()
 
-#Custom theme [YES]/[NO] for inquirer checkboxes
-class CustomYesNo(Default):
-
-    def __init__(self):
-        super().__init__()
-        self.Checkbox.selected_icon = "[YES]"
-        self.Checkbox.unselected_icon = "[NO]"
-        self.Checkbox.selection_icon = " "
-        self.List.selection_cursor = " "
-
 def askoption():
     console.rule("[green][blink]What information you want to write into Info.txt[/blink]"+"\n"+"[green]Space - Select Enter - Pr")
-
     questions = [inquirer.Checkbox( 
         'options',
-        choices=['CPU', 'RAM', 'Architecture', 'CPU Family', 'Machine type', 'System','Computer name'],
-    )]
-    
-    optanswers = inquirer.prompt(questions, theme=CustomYesNo())
+        choices=['CPU', 'CPU Cores','CPU Family', 'Architecture', 'Machine type', 'RAM','IP-Address','Disk Partitions', 'System','Computer name'],)]  
+    optanswers = inquirer.prompt(questions)
     return optanswers
 
-
-def cpuinfowrite(dataFile): 
-    dataFile.write("CPU: "+str(cpuinfo.get_cpu_info()['brand_raw'])+"\n")
-def raminfowrite(dataFile):
-    dataFile.write("RAM: "+str(round(psutil.virtual_memory().total / (1024.0 **3)))+" GB"+"\n")
-def architinfowrite(dataFile):
-    dataFile.write("Architecture: "+str(platform.architecture())+"\n")
-def sysinfowrite(dataFile):
-    dataFile.write("System: "+str(platform.platform())+"\n")
-def machinetypeinfowrite(dataFile):
-    dataFile.write("Machine type: "+str(platform.machine())+"\n")
-def cpufamily(dataFile):
-    dataFile.write("CPU Family: "+str(platform.processor())+"\n")
-def compnameinfowrite(dataFile):
-    dataFile.write("Computer name: "+str(platform.node())+"\n")
-
+def cpuInfoWrite(dataFile): 
+    dataFile.write("----------CPU----------\n\t"+str(cpuinfo.get_cpu_info()['brand_raw'])+"\n\n")
+def ramInfoWrite(dataFile):
+    dataFile.write("----------RAM----------\n\t"+str(round(psutil.virtual_memory().total / (1024.0 **3)))+" GB"+"\n\n")
+def architInfoWrite(dataFile):
+    dataFile.write("----------Architecture----------\n\t"+str(platform.architecture())+"\n\n")
+def sysInfoWrite(dataFile):
+    dataFile.write("----------System----------\n\t"+str(platform.platform())+"\n\n")
+def machinetypeInfoWrite(dataFile):
+    dataFile.write("----------Machine type----------\n\t"+str(platform.machine())+"\n\n")
+def cpuFamilyInfoWrite(dataFile):
+    dataFile.write("----------CPU Family----------\n\t"+str(platform.processor())+"\n\n")
+def compnameInfoWrite(dataFile):
+    dataFile.write("----------Computer name----------\n\t"+str(platform.node())+"\n\n")
+def cpuCoresInfoWrite(dataFile):
+    dataFile.write("----------CPU Cores----------\n"+"\tPhysical cores: "+str(psutil.cpu_count(logical=False))+"\n")
+    dataFile.write("\tLogical cores: "+str(psutil.cpu_count(logical=True))+"\n")
+    dataFile.write("\tMax Frequency: "+str(psutil.cpu_freq().max)+"\n\n")
+def ipInfoWrite(dataFile):
+    dataFile.write("----------IP-Address----------\n\t" + str(socket.gethostbyname(socket.gethostname()))+"\n\n")
+def macInfoWrite(dataFile):
+    partitions = psutil.disk_partitions()
+    dataFile.write("----------Disk Partitions----------\n")
+    for partition in partitions:
+        dataFile.write("\tDisk: " + str(partition.device)+"\n")
+        dataFile.write("\tTotla size: " +str(round((psutil.disk_usage(partition.mountpoint).total)/ (1024.0 **3)))+" GB"+"\n")
+    dataFile.write("\n")
 
 def gatheroptions(optanswers, dataFile):
     with console.status("Gathering information...", spinner='dots6'):
         for options in optanswers['options']:
             if options == "CPU": 
-                    cpuinfowrite(dataFile)
+                cpuInfoWrite(dataFile)
             if options == "RAM": 
-                    raminfowrite(dataFile)
+                ramInfoWrite(dataFile)
             if options == "CPU Family":
-                    cpufamily(dataFile)
+                cpuFamilyInfoWrite(dataFile)
             if options == "System":
-                    sysinfowrite(dataFile)
+                sysInfoWrite(dataFile)
             if options == "Architecture":
-                    architinfowrite(dataFile)
+                architInfoWrite(dataFile)
             if options == "Machine type":
-                    machinetypeinfowrite(dataFile)
+                machinetypeInfoWrite(dataFile)
             if options == "Computer name":
-                    compnameinfowrite(dataFile)
-
+                compnameInfoWrite(dataFile)
+            if options == "CPU Cores":
+                cpuCoresInfoWrite(dataFile)
+            if options == "IP-Address":
+                ipInfoWrite(dataFile)
+            if options == "Disk Partitions":
+                macInfoWrite(dataFile)
     console.rule("[green]All selected info was writen to (Info.txt)")
 
-def main():
-    
+def main():  
     dataFile=open("Info.txt", "w")
     gatheroptions(askoption(), dataFile)
     dataFile.close

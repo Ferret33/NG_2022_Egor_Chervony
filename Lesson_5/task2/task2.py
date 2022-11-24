0e6c00292095d7092f91f2d708ffb090eb98682c
Yehor Chervony <chervony.egor@gmail.com>
2022-11-24 16:41:46 +0200

    Lection 5 - Task 2| Web simple calculator bt flask

---
+++ Lesson_5/task2/task2.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask("Calculator")
+
+@app.route('/')
+def index():
+    # test = request.args.get('firstvalue')
+    # prfloat(test)
+    # res =0
+    # res= float(float(request.args.get('firstvalue'))+float(request.args.get('secondvalue')))
+    return render_template("index.html")
+    # return render_template("index.html")
+@app.route('/pls')
+def pls():
+    res= float(float(request.args.get('firstvalue'))+float(request.args.get('secondvalue')))
+    return render_template("index.html", res =res)
+@app.route('/min')
+def min():
+    res= float(float(request.args.get('firstvalue'))-float(request.args.get('secondvalue')))
+    return render_template("index.html", res =res)
+
+@app.route('/multy')
+def multy():
+    res= float(float(request.args.get('firstvalue'))*float(request.args.get('secondvalue')))
+    return render_template("index.html", res =res)
+@app.route('/dev')
+def dev():
+    if int(request.args.get('secondvalue'))==0:
+        res="ERROER devide by 0"
+    else:
+        res= float(float(request.args.get('firstvalue'))/float(request.args.get('secondvalue')))
+    return render_template("index.html", res =res)
+@app.route('/square')
+def square():
+    res= float(float(request.args.get('firstvalue'))**float(request.args.get('secondvalue')))
+    return render_template("index.html", res =res)
+@app.route('/squareroot')
+def squareroot():
+    if int(request.args.get('secondvalue'))==0:
+        res="ERORR no 0 root"
+    else:
+        res= float(float(request.args.get('firstvalue'))**(1/float(request.args.get('secondvalue'))))
+    return render_template("index.html", res =res)
+
+app.run(host="0.0.0.0", port=8888)

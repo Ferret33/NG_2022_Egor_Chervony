3b84270636d63d549b894d6d69a02a3a23402366
Yehor Chervony <chervony.egor@gmail.com>
2022-12-26 19:16:36 +0200

    Admin panel Open option finised

--- Lesson_5/task4/newoption.py
+++ Lesson_5/task4/newoption.py
@@ -3,9 +3,11 @@ def prepareFile(name):
     file = open(str(name),"r")
     return file
 def prepareNewslist(name):
+    file = prepareFile(name)
     newslst = []
-    for lines in prepareFile(name).readlines():
+    for lines in file.readlines():
         newslst.append(lines)
+    file.close
     return newslst
 
 def optionsNewsGenerator(name):
@@ -15,20 +17,26 @@ def optionsNewsGenerator(name):
     for lines in file.readlines():
         newslst.append(lines)
     line = 1
-    iteration = 1
+    iteration = 0
     while iteration < len(newslst)/4:
         newsoptions+="<option>"+str(newslst[line][4:-6])+"</option>\n"
         line+=4
         iteration+=1
     file.close
-    # print(newsoptions)
     return newsoptions
 
 def searchNews(data, name):
-    newslst = []
-    for lines in prepareFile(name).readlines():
-        newslst.append(lines)
-    print(newslst)
-    if data in newslst:
-        return "ok"
-            
+    newslst=[]
+    file = prepareFile(name)
+    for line in file.readlines():
+         newslst.append(line)
+    for element in newslst:
+        if str(element)==str(data)+"\n":
+            iteration = 0
+            news=""
+            ind=-1
+            while iteration<4:
+                news+=str(newslst[newslst.index(data+"\n")+ind])
+                ind+=1
+                iteration+=1
+            return news

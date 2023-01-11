047126aa9a8256654eb79e99bb0d0e59cac023d1
Yehor Chervony <chervony.egor@gmail.com>
2023-01-11 13:53:37 +0200

    Added more logs to console

--- Lesson_7/task1/getherimageshelper.py
+++ Lesson_7/task1/getherimageshelper.py
@@ -11,7 +11,6 @@ def downloadImg(site,Url):
     'User-Agent': '...',
     'referer': 'https://...'
     }
-    print(site)
     req = requests.get(Url, headers=headers)
     img = [".img",".jpg", ".jpeg", ".gif", ".png",".svg"]
     links=[]
@@ -21,31 +20,48 @@ def downloadImg(site,Url):
     path="temp/"+site
     for files in os.scandir(path):
         os.remove(files)
-    with console.status("Downloading...", spinner="monkey"):
+    # if not req:
+    #     print ("Error, no conection with site")
+    # else:
+    #     pass
+    print("Conecting to site...")
+    if req.status_code == 200:
+        print(site)
+        print("200")
         for line in html:
             for ex in img:
                 if ex in line:
-                    if line[0:4] == "http" and line[-4:] == ex:
+                    if line[0:4] == "http" and line[-4:] or line[-5:]  == ex:
                         links.append(line)
-                    elif line[0] == "/" and line[-4:] == ex:
+                    elif line[0] == "/" and line[-4:] or line[-5:] == ex:
                         if Url[-1] == "/":
                             links.append(Url+ line[1:])
                         else:
                             links.append(Url+line)   
-                    elif line[0] != "/" and line[-4:] == ex:
+                    elif line[0] != "/" and line[-4:] or line[-5:] == ex:
                         if Url[-1] == "/":
                             links.append(Url+line)
                         else:
                             links.append(Url+"/"+line) 
-                    for element in links:
-                            try:
-                                with open("temp/"+site+"/"+str(line.split("/")[-1]),"wb") as imgfile:
-                                    imgContent = requests.get(str(element)).content
-                                    imgfile.write(imgContent)
-                                    imgfile.close
-                            except:
-                                pass
+        print ("Find "+str(len(links))+" image(s)" )
+        # print("Start downloading...")
+        imgcounter=1
+        with console.status("Downloading...", spinner="monkey"):
+            for element in links:
+                try:
+                    with open("temp/"+site+"/"+str(element.split("/")[-1]),"wb") as imgfile:
+                        imgContent = requests.get(str(element)).content
+                        imgfile.write(imgContent)
+                        imgfile.close
+                        print("Image "+ str(element.split("/")[-1])+ " was saved!")
+                    print(str(imgcounter)+"/"+str(len(links)))
+                    imgcounter+=1    
+                except:
+                    pass
 
+    else:
+        print ("wrong")
+    
 def zippingImg(site):
     if not os.path.exists("zips/"+site):
         with zipfile.ZipFile("zips/"+str(site)+".zip","w") as zip:
@@ -58,7 +74,6 @@ def zippingImg(site):
             for files in os.walk("temp/"+site):
                 for elements in files[2]:
                     zip.write(elements)
-    
 
 def downloadZippingInThreads(site,Url):
     threads=[]
